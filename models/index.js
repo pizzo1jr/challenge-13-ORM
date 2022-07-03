45dba586963974506db8e1eb500cc65557876123
pizzo1jr <102200085+pizzo1jr@users.noreply.github.com>
2022-07-03 19:10:36 -0400

    commit

---
+++ models/index.js
@@ -0,0 +1,34 @@
+// import models
+const Product = require('./Product');
+const Category = require('./Category');
+const Tag = require('./Tag');
+const ProductTag = require('./ProductTag');
+
+// Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
+
+// Categories have many Products
+Product.belongsToMany(Product, {
+  foreignKey: 'product_id',
+});
+
+// Products belongToMany Tags (through ProductTag)
+Tag.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: 'product_id',
+});
+
+// Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: 'tag_id',
+});
+
+module.exports = {
+  Product,
+  Category,
+  Tag,
+  ProductTag,
+};
